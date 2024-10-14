9ae61f96a755ba98204ae05882d1e1794ecbf928
Saranya Shankar <saranya.shankar@yahoo.com>
2024-10-14 13:36:36 -0700

    Added code

---
+++ js/script.js
@@ -0,0 +1,28 @@
+//Global variables
+
+let score1;
+let score2;
+let score3;
+let total = 0;
+let average;
+
+//Get 3 scores from user and add them together
+score1 = prompt("What is the first score?");
+total += parseFloat(score1);
+
+score2 = prompt("What is the second score?");
+total += parseFloat(score2);
+
+score3 = prompt("What is the third score?");
+total += parseFloat(score3);
+
+//Calculate the average
+average = total / 3;
+
+//Display the result
+
+document.write(`<h1>The Test Scores Application</h1>`);
+document.write(`The first score was ${score1} <br/>`);
+document.write(`The first score was ${score2} <br/>`);
+document.write(`The first score was ${score3} <br/>`);
+document.write(`The average of the three scores is ${total.toFixed(2)}`);
